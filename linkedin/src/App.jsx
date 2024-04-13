4054d7b4075bcc32ce0fee2ff77dfd8fd06c083d
iasonasxrist <iasonasxrist@gmail.com>
2024-04-13 14:31:56 +0300

    Initial stylings

---
+++ linkedin/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App"></div>
+    </>
+  );
+}
+
+export default App;
