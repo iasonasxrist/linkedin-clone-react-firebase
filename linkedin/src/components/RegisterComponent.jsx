024d3b52f8be8446afaaf4c29ca56786aaede939
iasonasxrist <iasonasxrist@gmail.com>
2024-04-28 16:04:41 +0300

    Minor fixes

--- linkedin/src/components/RegisterComponent.jsx
+++ linkedin/src/components/RegisterComponent.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { postUserData } from "../api/FirestoreAPI";
+import { useState } from "react";
+// import { postUserData } from "../api/FirestoreAPI";
 import { useNavigate } from "react-router-dom";
+import { RegisterAPI } from "../api/AuthAPI";
+import { toast } from "react-toastify";
+import Linkedin from "../assets/linkedinLogo.png";
 
 const RegisterComponent = () => {
   const [credentials, setCredentials] = useState({});
@@ -11,8 +14,9 @@ const RegisterComponent = () => {
     try {
       // Add toast pop up messages
       const resp = await RegisterAPI(credentials.email, credentials.password);
+      console.log("🚀 ~ register ~ resp:", resp);
       toast.success("Registered in LinkedIn");
-      navigate("/home")
+      navigate("/home");
       // postUserData({
       //   userID: uuid(),
       //   name: credentials.name,
@@ -22,64 +26,63 @@ const RegisterComponent = () => {
       console.log(err);
       toast.error("Please Check your Credentials");
     }
+  };
 
-  return (  
-    
-      <div className="login-wrapper">
-        <img
-          src={LinkedinLogo}
-          className="linkedinLogo"
-        />
+  return (
+    <div className="login-wrapper">
+      <img src={Linkedin} className="linkedinLogo" />
 
-        <div className="login-wrapper-inner">
-          <h1 className="heading">Register</h1>
-          <p className="sub-heading">Stay updated on your professional world</p>
+      <div className="login-wrapper-inner">
+        <h1 className="heading">Register</h1>
+        <p className="sub-heading">Stay updated on your professional world</p>
 
-          <div className="auth-inputs">
-        <input
-        type="text"
-        className="common-input"
-        placeholder="zhiskas"
-        onChange={(event) => {
-          setCredentials({ ...credentials, name: event.target.value });
-        }}
-        />          
-        
-        <input
-          type="email"
-          className="common-input"
-          placeholder="Email or Phone"
-          onChange={(event) => {
-            setCredentials({ ...credentials, email: event.target.value });
-          }}
-        />
+        <div className="auth-inputs">
+          <input
+            type="text"
+            className="common-input"
+            placeholder="zhiskas"
+            onChange={(event) => {
+              setCredentials({ ...credentials, name: event.target.value });
+            }}
+          />
 
-        <input
-          type="password"
-          className="common-input"
-          placeholder="Password"
-          onChange={(event) => {
-            setCredentials({
-              ...credentials,
-              password: event.target.value,
-            });
-          }}
-        />
-          </div>       
+          <input
+            type="email"
+            className="common-input"
+            placeholder="Email or Phone"
+            onChange={(event) => {
+              setCredentials({ ...credentials, email: event.target.value });
+            }}
+          />
 
-          <button onClick={register} className="login-btn">
-            Signin
-          </button>
+          <input
+            type="password"
+            className="common-input"
+            placeholder="Password"
+            onChange={(event) => {
+              setCredentials({
+                ...credentials,
+                password: event.target.value,
+              });
+            }}
+          />
         </div>
+        <button onClick={register} className="login-btn">
+          Agree & Join
+        </button>
       </div>
-    
+
+      <hr className="hr-text" data-content="or" />
+      <div className="google-btn-container">
+        <p className="go-to-signup">
+          Already on LinkedIn?{" "}
+          <span className="join-now" onClick={() => navigate("/login")}>
+            Sign in
+          </span>
+        </p>
+      </div>
+    </div>
   );
-}
-
-
+};
 
 export default RegisterComponent;
-
-//   GoogleButton onClick={googleSignIn}
-//   Already on Linkedin ?  Navigate to Signin
-
