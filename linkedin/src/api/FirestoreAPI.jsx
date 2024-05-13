525362d9fc4bf39f27799ad9b6226d13ea8cdec9
iasonasxrist <iasonasxrist@gmail.com>
2024-05-13 20:22:16 +0300

    Firestore API

--- linkedin/src/api/FirestoreAPI.jsx
+++ linkedin/src/api/FirestoreAPI.jsx
@@ -1,12 +1,36 @@
-import { addDoc } from "firebase/firestore"
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { firestore } from "../firebaseConfig.js";
+import { toast } from "react-toastify";
 
-export const postUserData = (object) =>{
-    addDoc(userRef, object)
-    .then(()=>{
+let dbRef = collection(firestore, "posts");
 
+export const postStatus = (status) => {
+  let object = { status: status };
+
+  addDoc(dbRef, object)
+    .then(() => {
+      toast.success("Document added successfully!");
     })
-    .catch((error)=>{
-        console.log(error);
-    })
+    .catch((err) => {
+      console.log(err);
+      toast.error("Something may went wrong!");
+    });
+};
 
-}
+export const getStatus = (setAllStatus) => {
+  onSnapshot(dbRef, (response) => {
+    setAllStatus(
+      response.docs.map((docs) => {
+        return { ...docs.data(), id: docs.id };
+      })
+    );
+  });
+};
+
+// export const postUserData = (object) => {
+//   addDoc(userRef, object)
+//     .then(() => {})
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
