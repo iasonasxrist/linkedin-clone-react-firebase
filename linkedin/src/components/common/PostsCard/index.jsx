01a13f09a9e982325e492b7390baa13067163e79
iasonasxrist <iasonasxrist@gmail.com>
2024-06-16 16:13:30 +0300

    PostCard update

--- linkedin/src/components/common/PostsCard/index.jsx
+++ linkedin/src/components/common/PostsCard/index.jsx
@@ -1,19 +1,18 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Modal } from "antd";
 import { BsPencil, BsTrash } from "react-icons/bs";
 import "./index.scss";
 
-export default function PostsCard(props) {
+export default function PostsCard({ posts, id }) {
   let navigate = useNavigate();
 
   return (
-    <div className="posts-card" key={props.id}>
+    <div className="posts-card" key={id}>
       <div className="post-image-wrapper">
         <div className="action-container">
           <BsPencil size={20} className="action-icon" onClick={""} />
           <BsTrash size={20} className="action-icon" onClick={""} />
-          <p>{props.posts.status}</p>
+          <p>{posts.timeStamp}</p>
         </div>
       </div>
     </div>
