6a9274547528dd83c1cfb1ba3ef23e1544a19b87
iasonasxrist <iasonasxrist@gmail.com>
2024-07-06 14:12:37 +0300

    Loader

--- linkedin/src/helper/Loader.jsx
+++ linkedin/src/helper/Loader.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import { Space, Spin } from "antd";
+import { Space, Spin } from "antd";
 
 export default function Loader() {
   return (
     <div className="loader">
       <p>Loading..Please wait...</p>
-      {/* <Space size="middle">
+      <Space size="middle">
         <Spin size="large" />
-      </Space> */}
+      </Space>
     </div>
   );
 }
